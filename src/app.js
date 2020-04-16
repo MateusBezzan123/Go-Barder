fdfc9e6a2393047f21f138fa8e05088dcee385eb
unknown <mateus.bezzan@aluno.unievangelica.edu.br>
2020-04-16 20:47:49 -0300

    Projeto de um salão de beleza

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+// eslint-disable-next-line linebreak-style
+import routes from './routes';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
