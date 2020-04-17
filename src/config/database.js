690d5cc0ff919ba6c6571ab3ac61c25f414634af
unknown <mateus.bezzan@aluno.unievangelica.edu.br>
2020-04-16 21:17:51 -0300

    Configuração do sequilaze

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dilect: 'postegres',
+    host: 'localhost',
+    username: 'postegres',
+    password: 'root',
+    database: 'gobarber',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+};
