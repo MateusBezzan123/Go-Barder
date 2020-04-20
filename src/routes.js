cb7b6e88bed1101ffaead35c7b35caff1ba40309
unknown <mateus.bezzan@aluno.unievangelica.edu.br>
2020-04-20 16:47:09 -0300

    Middleware de autenticação

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,14 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionControler from './app/controllers/SessionController';
+import authMiddlewares from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionControler.store);
+
+routes.use(authMiddlewares);
+
+routes.put('/users', UserController.update);
 export default routes;
