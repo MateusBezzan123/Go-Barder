bc0221f72963a9bb84f8b66a84c4823070bf45f7
unknown <mateus.bezzan@aluno.unievangelica.edu.br>
2020-04-20 11:07:01 -0300

    Cadastro de usuários

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-    const user = await User.create({
-        name: 'Mateus Correia Bezzan',
-        email: 'mateusbhn998@gmail.com',
-        password_hash: '1238546189',
-    });
-    return res.json(user);
-});
+routes.post('/users', UserController.store);
+
 export default routes;
